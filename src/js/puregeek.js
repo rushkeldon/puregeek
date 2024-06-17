5e0f421dd300f60a00a4571b397ba830bd6b33dd
Keldon Rush <keldon@spiral9.com>
2024-06-17 11:08:44 -0700

    main - incremental

--- src/js/puregeek.js
+++ src/js/puregeek.js
@@ -1,1 +1,12 @@
 console.log( 'js/puregeek.js loaded' );
+
+window.onload = () => {
+  setTimeout( () => {
+    console.log( 'first timeout' );
+    window.scrollTo(0, 30);
+    setTimeout( () => {
+      console.log( 'second timeout' );
+      window.scrollTo(0, 1);
+    }, 1 );
+  }, 250);
+}
